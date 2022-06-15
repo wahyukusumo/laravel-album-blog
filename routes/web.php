d9339394d3c1a20bad334f832ea1eadeb6f50b25
Wahyu Kusumo <wahyukusumo32@gmail.com>
2022-06-15 22:33:36 +0700

    Fix index routes

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::resource('/', \App\Http\Controllers\PostController::class);
 Route::resource('/posts', \App\Http\Controllers\PostController::class);
