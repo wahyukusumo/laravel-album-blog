2c2cc920d9e57bf5ab0552330d0ac8e9f5f92352
Wahyu Kusumo <wahyukusumo32@gmail.com>
2022-06-18 00:38:06 +0700

    Add my info and pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layout')
 
 @section('content')
+
+    <div class="alert alert-info alert-dismissible fade show" role="alert">
+        Website CRUD untuk tugas kelas <strong>FS204B Pemrograman 2</strong>. Dibuat oleh <strong>Wahyu Kusumoraharjo (2022446263)</strong>. Repositori untuk website ini bisa dilihat <a href="https://github.com/wahyukusumo/Programming_2" target="_blank" class="alert-link">di sini</a>.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
         @forelse ($posts as $post)
             <div class="col">
@@ -50,12 +56,17 @@
             <div class="alert alert-danger">
                 Data Post belum Tersedia.
             </div>
-        @endforelse
+        @endforelse        
     </div>
 
+    {{-- PAGINATION --}}
+    <div class="col-md-12 my-3">
+        {{ $posts->links() }}    
+    </div>
+    
     <footer class="text-muted py-5">
         <div class="container">
-            <p class="mb-1 text-center">
+            <p class="text-center">
                 <a href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
@@ -64,17 +75,4 @@
             </p>
         </div>
     </footer>
-
-    <script>
-        //message with toastr
-        @if(session()->has('success'))
-        
-            toastr.success('{{ session('success') }}', 'BERHASIL!'); 
-
-        @elseif(session()->has('error'))
-
-            toastr.error('{{ session('error') }}', 'GAGAL!'); 
-            
-        @endif
-    </script>
 @endsection
